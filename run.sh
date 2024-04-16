3002a3e1e49990024574f86b28af6a5b74ac1f18
jrunge <jrunge@precisionplanting.com>
2024-04-16 16:44:25 -0500

    Update with GLFW window

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ echo ""
 echo "Running the program..."
 echo ""
 ./bin/Debug/vulk.exe
-echo ""
-echo "Press any key to exit..."
-read -n 1 -s
+# echo ""
+# echo "Press any key to exit..."
+# read -n 1 -s
