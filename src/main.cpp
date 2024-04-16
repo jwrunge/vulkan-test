1323b4bad09d4830748582c8bb5b130c29b9dc65
jrunge <jrunge@precisionplanting.com>
2024-04-16 16:27:51 -0500

    Project setup done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
+#include <vulkan/vulkan.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+
+    }
+
+    void cleanup() {
+
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
