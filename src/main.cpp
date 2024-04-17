9e0cb2db462dd30e6b4d38c93dd323bb71f049eb
jrunge <jrunge@precisionplanting.com>
2024-04-17 12:41:53 -0500

    Extension checking

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const char* APP_NAME = "Hello Triangle";
 
 class HelloTriangleApplication {
 public:
@@ -19,6 +21,7 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -29,7 +32,62 @@ private:
     }
 
     void initVulkan() {
+        createInstance();
+    }
 
+    void createInstance() {
+        VkApplicationInfo appInfo{
+            .sType =                VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pApplicationName =     APP_NAME,
+            .applicationVersion =   VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName =          "No Engine",
+            .engineVersion =        VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion =           VK_API_VERSION_1_0,
+        };
+
+        VkInstanceCreateInfo createInfo{
+            .sType =                    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pApplicationInfo =         &appInfo,
+        };
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // Handle extensions
+        #ifdef __APPLE__
+            std::vector<const char*> requiredExtensions;
+            for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+                requiredExtensions.emplace_back(glfwExtensions[i]);
+            }
+            requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+            createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+            createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+            createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+            std::cout << "Using portability extension for Apple" << std::endl;
+        #endif
+
+        //Optional extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions:" << std::endl;
+        for(const auto& extension : extensions) {
+            std::cout << extension.extensionName << std::endl;
+        }
+
+        // Create instance
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance!");
+        }
     }
 
     void mainLoop() {
@@ -39,6 +97,7 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
