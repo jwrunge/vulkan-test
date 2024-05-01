2ad51f08719993b0e793d9dc5cb16f00fe19a9da
jrunge <jrunge@precisionplanting.com>
2024-04-30 22:34:15 -0500

    Setting up for cmake plugin

--- build.sh
+++
@@ -1,2 +0,0 @@
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
-cmake --build build
--- scripts/build.conditional.sh
+++
@@ -1,9 +0,0 @@
-if ./build.sh && [[ $? -eq 0 ]] ; then
-    echo ""
-    echo "Build succeeded!"
-    echo ""
-else
-    echo ""
-    echo "Build failed. Press any key to exit..."
-    read -n 1 -s
-fi
