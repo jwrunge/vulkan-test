d72b867aa97f41104140b986ab4a7a2eb52ca92e
jrunge <jrunge@precisionplanting.com>
2024-10-01 00:07:31 -0500

    Setup with conan

---
+++ build.sh
@@ -0,0 +1,6 @@
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
+cmake --build . --config Release
+cd ..
+sleep 100
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use renderer::run;
-
-fn main() {
-    run();
-}
